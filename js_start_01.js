ebb4bfdccdad7ef7020f4476d67108f2ccbe1898
babybee03 <flowerysora@hanmail.net>
2024-01-02 17:01:47 +0900

    a

--- js_start_01.js
+++ js_start_01.js
@@ -1,43 +1,14 @@
-var topleft = document.querySelector('#topleft');
-var topright = document.querySelector('#topright');
-var bottomleft = document.querySelector('#bottomleft');
-var bottomright = document.querySelector('#bottomright');
-var container = document.querySelector('.container');
+var square = document.querySelector(".square");
+var menu = document.querySelector('.menu');
 var number = 0;
-
-function loveIsOpenDoor() {
+function openTheDoor() {
     number++;
-    if (number % 6 === 0) {
-        topleft.classList.remove('open');
-        topright.classList.remove('open');
-        bottomleft.classList.remove('open');
-        bottomright.classList.remove('open');
-    } else  if (number % 6 === 1) {
-        topleft.classList.add('open');
-        topright.classList.remove('open');
-        bottomleft.classList.remove('open');
-        bottomright.classList.remove('open');
-    } else if (number % 6 === 2 ){
-        topleft.classList.remove('open');
-        topright.classList.add('open');
-        bottomleft.classList.remove('open');
-        bottomright.classList.remove('open');
-    } else if (number % 6 === 3) {
-        topleft.classList.remove('open');
-        topright.classList.remove('open');
-        bottomleft.classList.add('open');
-        bottomright.classList.remove('open');
-    } else if (number % 6 === 4){
-        topleft.classList.remove('open');
-        topright.classList.remove('open');
-        bottomleft.classList.remove('open');
-        bottomright.classList.add('open');
-    } else {
-        topleft.classList.add('open');
-        topright.classList.add('open');
-        bottomleft.classList.add('open');
-        bottomright.classList.add('open');
-    }
+ if (number % 2 == 0) {
+    menu.classList.remove('open');
+ } else {
+    menu.classList.add('open');
+ }
 }
+square.addEventListener('click', openTheDoor);
 
-container.addEventListener('click', loveIsOpenDoor);
+
