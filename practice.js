cd3d2cfd7e2e1f8152e3df210ed187c4346024f6
babybee03 <flowerysora@hanmail.net>
2024-01-02 18:07:30 +0900

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,13 +1,29 @@
-var id = 'jordy';
-var password = '0315';
+/*
+var id = 'sora';
+var pw = '0315';
 
-var putId = prompt('당신의 아이디를 입력하세요');
+var putId = prompt('아이디를 입력하세요');
 
-
-if (putId == 'jordy') {
-    var putPw = prompt('당신의 비밀번호를 입력하세요');
-    if (putPw == '0315') {
-        document.write(id,'님, 로그인되었습니다');
+if (putId == 'sora') {
+    var putPw = prompt('비밀번호를 입력하세요');
+    if (putPw =='0315') {
+        document.write(id+"님, 안녕하세요");
+    } else {
+        alert('비밀번호를 다시 입력해주세요');
+        location.reload();
+    }
+} else {   
+    alert('아이디를 다시 입력해주세요');
+    location.reload();
+}
+*/
+var id = 'tree';
+var pw = '1234';
+var user_id = prompt('아이디는?');
+var user_pw = prompt('비밀번호는?');
+if (id == user_id) {
+    if(pw == user_pw) {
+        document.write(user_id+"님, 반갑습니다");
     } else {
         alert('비밀번호가 일치하지 않습니다.');
         location.reload();
