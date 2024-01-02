5202b7608facf331c72bac9b845d31a34c2f417d
babybee03 <flowerysora@hanmail.net>
2024-01-02 12:13:27 +0900

    1.2

--- js_start_01.js
+++ js_start_01.js
@@ -1,21 +1,43 @@
-var position = 0;
-var main_w = document.querySelector("#main_box");
-var left_w = document.querySelector("#left_box");
-var right_w = document.querySelector("#right_box");
+var topleft = document.querySelector('#topleft');
+var topright = document.querySelector('#topright');
+var bottomleft = document.querySelector('#bottomleft');
+var bottomright = document.querySelector('#bottomright');
+var container = document.querySelector('.container');
+var number = 0;
 
-function ani_slide() {
-    position++;
-    if (position % 3 == 0) {
-        left_w.classList.remove('open');
-        right_w.classList.remove('open');
-    } else if (position % 3 == 1) {
-        left_w.classList.add('open');
-        right_w.classList.add('open');
+function loveIsOpenDoor() {
+    number++;
+    if (number % 6 === 0) {
+        topleft.classList.remove('open');
+        topright.classList.remove('open');
+        bottomleft.classList.remove('open');
+        bottomright.classList.remove('open');
+    } else  if (number % 6 === 1) {
+        topleft.classList.add('open');
+        topright.classList.remove('open');
+        bottomleft.classList.remove('open');
+        bottomright.classList.remove('open');
+    } else if (number % 6 === 2 ){
+        topleft.classList.remove('open');
+        topright.classList.add('open');
+        bottomleft.classList.remove('open');
+        bottomright.classList.remove('open');
+    } else if (number % 6 === 3) {
+        topleft.classList.remove('open');
+        topright.classList.remove('open');
+        bottomleft.classList.add('open');
+        bottomright.classList.remove('open');
+    } else if (number % 6 === 4){
+        topleft.classList.remove('open');
+        topright.classList.remove('open');
+        bottomleft.classList.remove('open');
+        bottomright.classList.add('open');
     } else {
-        left_w.classList.remove('open');
-        right_w.classList.add('open');
+        topleft.classList.add('open');
+        topright.classList.add('open');
+        bottomleft.classList.add('open');
+        bottomright.classList.add('open');
     }
 }
 
-main_w.addEventListener('click',ani_slide);
-left_w.addEventListener('click',ani_slide);
+container.addEventListener('click', loveIsOpenDoor);
