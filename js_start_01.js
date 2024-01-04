83bd739f44677be88552b269ed08a346b73f9e63
babybee03 <flowerysora@hanmail.net>
2024-01-04 20:35:48 +0900

    1

--- js_start_01.js
+++ js_start_01.js
@@ -1,16 +1,4 @@
-var arr_01 = ["사당", "교대", "방배", "강남"];
-var arr_02 = ["신사", "압구정", "옥수"];
-var result = arr_01.join("-");
-console.log(result);
+var greenArray = ["교대", "방배", "강남"];
+var yellowArray = ["미금","정자","수서"];
 
-result = arr_01.concat(arr_02);
-console.log(result);
-
-result = arr_01.slice(1, 3);
-console.log(result);
-
-arr_01.sort();
-console.log(arr_01);
-
-arr_02.reverse();
-console.log(arr_02);
+greenArray
