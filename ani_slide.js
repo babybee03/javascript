82878a474e7c4edd4a3adcc29a6ea0d43a14976e
babybee03 <flowerysora@hanmail.net>
2024-01-09 18:03:44 +0900

    1

---
+++ ani_slide.js
@@ -0,0 +1,7 @@
+var menu = document.querySelector('.menu');
+var button = document.querySelector('.button');
+
+button.addEventListener('click', function(){
+    menu.classList.toggle('open');
+
+})
