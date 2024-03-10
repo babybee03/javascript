ee24150c1af89ab1318c4e48908baf45063a53bd
babybee03 <153714246+babybee03@users.noreply.github.com>
2024-03-10 23:32:21 +0900

    practice fade in

--- js_start_01.js
+++ js_start_01.js
@@ -1,38 +1,17 @@
-/*
-var all = document.querySelector('.all');
-var pic_1 = document.querySelector('#pic_1');
-var pic_2 = document.querySelector('#pic_2');
-var pic_3 = document.querySelector('#pic_3');
+const showBox = document.querySelectorAll(".showBox");
 
+const getTop = (element) => element.getBoundingClientRect().top;
 
-pic_1.addEventListener('click', function (){
-    pic_2.classList.add('up');
-    pic_1.classList.remove('up');
-})
+const fadeIn = (element) => {
+    let y = getTop(element);
 
-pic_2.addEventListener('click', function (){
-    pic_3.classList.add('up');
-    pic_2.classList.remove('up');
-})
-
-pic_3.addEventListener('click', function (){
-    pic_1.classList.add('up');
-    pic_3.classList.remove('up');
-})
-*/
-var all = document.querySelector('.all');
-var i=0;
-
-all.addEventListener('click',function(){
-    i++;
-    if(i % 3 == 1) {
-        all.classList.add('open_1');
-        all.classList.remove('open_2');
-    } else if (i % 3 == 2) {
-        all.classList.add('open_2');
-        all.classList.remove('open_1');
-    } else {
-        all.classList.remove('open_1');
-        all.classList.remove('open_2');
+    if(y < window.scrollY + window.innerHeight - 400) {
+        element.classList.add("on");
     }
-})
+};
+
+window.addEventListener("scroll", () => {
+    for(let element of showBox) {
+        fadeIn(element);
+    }
+});
